2a70647febd8ff963573d57ef96a45559096fa26
sanomanjiro15 <milana.0070@list.ru>
2023-03-15 18:32:13 +0300

    added wrong test_put_employee

--- tests/employees/test_put_employee_wrong_fields.py
+++ tests/employees/test_put_employee_wrong_fields.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.mark.parametrize("id_of_employee, name, role, exp_code", [
-    ("12", 1, [1, 2, 3], "admin", 422),
-    (6, [23], "Ivan", "student", 422),
-    ({"idk"}, 'student', "Ivan", [1, 2, 3], 422),
-    ([54], [1, 2, 3], "Ivan", 123, 422)
+    ( 1, [1, 2, 3], "admin", 422),
+    ([23], "Ivan", "student", 422),
+    ('idk', "Ivan", [1, 2, 3], 422),
+    ([54], "Ivan", 123, 422)
 ])
 def test_negative(employee_fixture, id_of_employee, name, role, exp_code):
     response = employee_fixture.api_client.employee.update_employee(id_of_employee, name, role)
