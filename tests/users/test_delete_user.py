d7f8b093b57e65dd2e48a0b60cd53efcf8d191d2
sanomanjiro15 <milana.0070@list.ru>
2023-03-15 18:08:06 +0300

    added delete_user test

--- tests/users/test_delete_user.py
+++ tests/users/test_delete_user.py
@@ -1,1 +1,4 @@
 # не моя часть
+def test_positive(user_fixture):
+    response = user_fixture.api_client.user.delete_user(user_fixture.user_id)
+    assert response.status_code == 200, "Статус код не соответствует ожидаемому"
